172af8213f55655544267f3c9c2810a5788b613b
Your Name <you@example.com>
2026-04-21 13:36:46 +0530

    added a file new_py

---
+++ # Bonuspoint calculator.py
@@ -0,0 +1,15 @@
+# Bonus point calculator
+print('welcome to bonus point calculator')
+q1=input("Are you participated in SPORTS(yes/no):")
+q2=input("Are you participated in ARTS(yes/no) :")
+q3=input("Are you participated in CLUBS(yes/no):")
+ 
+TotalBonus=0
+if q1=='yes':
+ TotalBonus +=5
+if q2=='yes':
+ TotalBonus +=10
+if q3=='yes':
+ TotalBonus +=3
+print(f"total bonus point:{TotalBonus}")
+#  To Find electricity bill
--- first.py
+++ first.py
@@ -1,19 +1,19 @@
-num1=float(input("Enter a no:"))
-num2=float(input("Enter a no:"))
-op=input("choose option(=,-,*,/):")
-if op=='+':
-    result=num1+num2
-elif op=='-':
-    result=num1-num2
-elif op=='*':
-    result=num1*num2
-elif op=='/':
-    if num2!=0:
-        result=num1/num2
-    else:
-        print("cannot divide by zero!")
-        exit()
-else:
-      print("invalid operator!")
-      exit()
-print("result:",result)
+# num1=float(input("Enter a no:"))
+# num2=float(input("Enter a no:"))
+# op=input("choose option(=,-,*,/):")
+# if op=='+':
+#     result=num1+num2
+# elif op=='-':
+#     result=num1-num2
+# elif op=='*':
+#     result=num1*num2
+# elif op=='/':
+#     if num2!=0:
+#         result=num1/num2
+#     else:
+#         print("cannot divide by zero!")
+#         exit()
+# else:
+#       print("invalid operator!")
+#       exit()
+# print("result:",result)
