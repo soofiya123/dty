a0d5e04bc5514aecc411790ddc6578fd8688dcd1
Your Name <you@example.com>
2026-04-23 12:32:15 +0530

    added a file hello1.py

--- hello1.py
+++ hello1.py
@@ -8,3 +8,11 @@ print(table['salary'].mean())
 print(table['name'].min())
 print(table['salary'].max())
 print(table['salary'].count())
+
+result=table.agg(
+totalsalary=('salary','sum'),
+avgsalary=('salary','mean'),
+firstEmployee=('name','min'),
+lastEmployee=('name','max'),
+)
+print(result)
